91bde3e8268fb140b05a177d89fda2ac6662bfdd
jmpealy <jmpealy@gmail.com>
2023-06-17 19:39:32 -0500

    modified readme file

--- #README file.py
+++ #README file.py
@@ -1,15 +1,15 @@
 #README file
-https://stackoverflow.com/questions/19852215/how-to-add-a-legend-to-matplotlib-pie-chart
+#https://stackoverflow.com/questions/19852215/how-to-add-a-legend-to-matplotlib-pie-chart
 
-https://www.geeksforgeeks.org/how-to-change-legend-font-size-in-matplotlib/
+#https://www.geeksforgeeks.org/how-to-change-legend-font-size-in-matplotlib/
 
-https://jakevdp.github.io/PythonDataScienceHandbook/04.06-customizing-legends.html
+#https://jakevdp.github.io/PythonDataScienceHandbook/04.06-customizing-legends.html
 
-https://pypi.org/project/dataframe-image/
+#https://pypi.org/project/dataframe-image/
 
-https://randomds.com/2021/12/23/visualize-and-save-full-pandas-dataframes-as-images/
+#https://randomds.com/2021/12/23/visualize-and-save-full-pandas-dataframes-as-images/
 
-https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+#https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
 
 
 
